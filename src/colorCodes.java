f8b2f87d9aaae51b32d7ac3eaeef7380bc8512fb
Saad <115335100+Saad-1719@users.noreply.github.com>
2024-01-04 11:07:54 +0500

    new class(colorCodes) added

---
+++ src/colorCodes.java
@@ -0,0 +1,12 @@
+public class colorCodes
+{
+    static final String brightPurple = "\u001B[38;5;207m";
+    static final String brightRed = "\u001B[38;5;196m";
+    static final String brightOrange = "\u001B[38;5;45m";
+    static final String pureWhite = "\u001B[97m";
+    static final String partyEmoji = "\uD83C\uDF89"; //🎉
+    static final String brightYellow = "\u001B[38;5;226m";
+    static final String brightPeach = "\u001B[38;5;9m";
+    static final String brightGreen = "\u001B[38;5;82m";
+
+}
